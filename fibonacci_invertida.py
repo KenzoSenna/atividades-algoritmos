354d59dd94252379cc77718382bbdead2dd0e588
Kenzo de Oliveira Senna <kenzoandblade@gmail.com>
2025-04-14 15:58:33 -0400

    Add files via upload

--- fibonacci_invertida.py
+++ fibonacci_invertida.py
@@ -1,12 +1,11 @@
 a = 0
 b = 1
-lista= [0, 1]
+lista = "0, 1"
 qeue_number = int(input("Digite até que número da sequência de Fibonacci voce quer percorrer?\n: "))
 for counter in range(qeue_number - 2):
     c = a + b
-    lista.append(c)
+    lista = lista + ","+" " + str(c)
     print(f"sequencia = {lista}")
     a = b
     b = c
-print("Sequência de fibonacci reversa: ")
-print(lista[::-1])
+print("Sequência de fibonacci reversa: ", lista[::-1])
